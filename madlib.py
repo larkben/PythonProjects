b4ce0973331e819c01f954ecb64e4682c7150171
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2020-08-08 22:46:09 -0500

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,7 @@
+# Madlib or Project One
+
+verb1 = input("Enter a verb ")
+noun1 = input("Enter a noun ")
+adjective1 = input("Enter a adjective ")
+
+print ("The big fish was " + verb1 + " into the " + noun1 + ". The fish is very " + adjective1 + ".")
