7b70e72d5559e594730ca67676ef89d72275a557
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2020-08-08 22:46:14 -0500

    Create text_adventure.py

---
+++ text_adventure.py
@@ -0,0 +1,20 @@
+# Project 3 a text based adventure
+import time
+import math
+
+def room_one():
+    print("-----------")
+    print("lD         l")
+    print("l          ------")
+    print("lD    T          l ")
+    print("l          ------")
+    print("-----------")
+
+def room_two():
+    print("        l----------l")
+    print("--------lD         l")
+    print("l             T    l")
+    print("--------l          l")
+    print("        lD         l")
+    print("        l-----------")
+
